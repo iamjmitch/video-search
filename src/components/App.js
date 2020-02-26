8ebeb70f1ddc968f8eda35e0894c2fe9766f9bb2
James Mitchell <iamjmitch1990@gmail.com>
2020-02-27 00:12:57 +1000

    end of day 26/02/20

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,22 @@ import SearchBar from './SearchBar';
 import Youtube from '../apis/Youtube';
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    Youtube.get('/search', {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await Youtube.get('/search', {
       params: {
         q: term
       }
     });
+    this.setState({ videos: response.data.items });
   };
 
-  state = {};
   render() {
     return (
       <div className=" ui container">
-        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <SearchBar onTermSubmit={this.onTermSubmit} />I have{' '}
+        {this.state.videos.length} Videos
       </div>
     );
   }
